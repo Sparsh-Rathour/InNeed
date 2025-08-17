53253ef0f4adfd4c02d3b02ac1383c68401c04f8
Sparsh-Rathour <85083391+Sparsh-Rathour@users.noreply.github.com>
2025-08-17 23:14:32 +0530

    Add files via upload

---
+++ project-IWP-master/includes/sidebar.php
@@ -0,0 +1,32 @@
+<div id="layoutSidenav_nav">
+
+    <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+        <div class="sb-sidenav-menu">
+            <div class="nav">
+                <div class="sb-sidenav-menu-heading">Core</div>
+                <a class="nav-link" href="loginform.php">
+                    <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                    Dashboard
+                </a>
+                <a class="nav-link" href="view_register_job_seeker.php">
+                    <div class="sb-nav-link-icon"><i class="fas fa-users"></i></div>
+                    Registered Job Seeker
+                </a>
+
+                <a class="nav-link" href="view_register_job_employer.php">
+                    <div class="sb-nav-link-icon"><i class="fas fa-users"></i></div>
+                    Registered Employers
+                </a>
+
+                <a class="nav-link" href="view_job.php">
+                    <div class="sb-nav-link-icon"><i class="fas fa-users"></i></div>
+                    Job Details
+                </a>
+            </div>
+        </div>
+        <div class="sb-sidenav-footer">
+            <div class="small">Logged in as:</div>
+            Admin
+        </div>
+    </nav>
+</div>
